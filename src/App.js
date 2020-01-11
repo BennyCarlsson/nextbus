603f3815a1f3a4c0bf6dacee89ab18ea224612c4
Benny <carlsson.benny93@gmail.com>
2020-01-11 14:55:42 +0100

    cleanup code and fetches data dynamically on time

--- src/App.js
+++ src/App.js
@@ -5,32 +5,13 @@ import { id } from "./supersecretfile"
 
 function App() {
   useEffect(() => {
-    fetch("https://api.vasttrafik.se:443/token", {
-      headers: {
-        Authorization: `Basic ${id}`,
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      method: "POST",
-      body: "grant_type=client_credentials"
-    }).then(res =>
-      res
-        .json()
-        .catch(error => console.error("Error:", error))
-        .then(response => {
-          const token = response.access_token
-          const now = new Date()
-          const date = now.toISOString().substr(0, 10)
-          const time = now.toLocaleTimeString().substr(0, 5)
-          const tripUrl = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014013213001&date=2020-01-10&time=21:03&format=json`
-          fetch(tripUrl, {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          }).then(res => res.json().then(response => console.log(response)))
-        })
-    )
+    const getTimeTable = async () => {
+      const data = await getData()
+      console.log(data)
+    }
+    getTimeTable()
   }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -39,6 +20,51 @@ function App() {
       </header>
     </div>
   )
+}
+
+const getData = async () => {
+  let data = {}
+  try {
+    data = fetchData()
+  } catch (error) {
+    console.log("Something went wrong fetching data", error)
+  }
+  return data
+}
+
+const fetchData = async () => {
+  const acessToken = await fetchAccessToken()
+  const { date, time } = getTime()
+  const tripUrl = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014013213001&date=${date}&time=${time}&format=json`
+  const response = await fetch(tripUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${acessToken}`
+    }
+  })
+  const data = await response.json()
+  return data.DepartureBoard
+}
+
+const fetchAccessToken = async () => {
+  const response = await fetch("https://api.vasttrafik.se:443/token", {
+    headers: {
+      Authorization: `Basic ${id}`,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "POST",
+    body: "grant_type=client_credentials"
+  })
+  const data = await response.json()
+  return data.access_token
+}
+
+const getTime = () => {
+  //Todo gets the buses from exactly now. Will it show late busses pass the time?
+  const now = new Date()
+  const date = now.toISOString().substr(0, 10)
+  const time = now.toTimeString().substr(0, 5)
+  return { date, time }
 }
 
 export default App
